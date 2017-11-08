804452838014dc3d57d53756b595760fae094632
Elijah <Elijah@vl7-0.nat.net.pu.ru>
2017-11-08 14:30:11 +0300

    Logic improvement for better TICK's process integration

--- gameserver/server.py
+++ gameserver/server.py
@@ -2,40 +2,42 @@ import asyncio
 import time
 import websockets
 import multiprocessing
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Manager
+
 
 
 class Player:
-    def __init__(self, token, websocket):
-        self.token = token
-        self.websocket = websocket
+    def __init__(self, token, Q):
+        # TODO Bind game object with connection object
+        self.game_object = None
+        self.Q = Q  # Players multiprocessing input Queue
 
 
-def server(input_q, output_q):
-    connections = {}  # A dict for saving connections
+def server(connections, output_q, Q):
 
     async def handler(websocket, path):
+
         # Handshake
         token = await websocket.recv()  # Receiving players token
-        # Linking reconnected player with his old object
-        if token in connections:
-            connections[token].websocket = websocket
+
+        if token in connections.keys():  # Reconnect alertion
             print("player {} reconnected".format(token))
         else:  # Saving new connection
-            connections[token] = Player(token, websocket)
+            connections[token] = Player(token, Q)
             print("player {} connected".format(token))
-        await websocket.send("HI")
+
+        await websocket.send("HI")  # Handshake end
 
         # Main loop
         while True:
             try:  # Connection check
                 await asyncio.wait_for(websocket.ping(), timeout=10)
-                # breaking connection if player disconnected
-            except (asyncio.TimeoutError):
+                # breaking connection if timeout or disconnection
+            except (asyncio.TimeoutError, websockets.exceptions.ConnectionClosed):
                 print("Player {} disconnected".format(token))
                 break
 
-            # starting to coroutines
+            # starting two coroutines
             a = asyncio.ensure_future(consumer(websocket, token))
             b = asyncio.ensure_future(producer(websocket))
             done, pending = await asyncio.wait(
@@ -50,34 +52,40 @@ def server(input_q, output_q):
         try:
             message = await websocket.recv()
             print("Got message: {}".format(message))
-            input_q.put([token, message])  # appending to the clients' inputs Q
+            # appending to the clients' inputs Q
+            connections[token].Q.put([token, message])
         except (websockets.exceptions.ConnectionClosed):
-            pass
             print("CONNECTION CLOSED")
 
     async def producer(websocket):
+        await asyncio.sleep(3)
         if not output_q.empty():
-            print("".join(output_q.get()))
+            tmp = output_q.get()
+            print(tmp)
+            # Information handling here
+            await websocket.send(str(tmp))
 
     # start_server = websockets.serve(handler, '10.147.17.206', 9190)
     start_server = websockets.serve(handler, 'localhost', 9000)
-
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
 
 
-def tick(input_q, output_q):
+def tick(connections, output_q):
     while True:
-        if not input_q.empty():
-            output_q.put(
-                ["SOME RESPONSE FOR: {}".format(" ".join(input_q.get()))])
+        for i in connections.keys():
+            if not connections[i].Q.empty():
+                output_q.put(
+                    ["SOME RESPONSE FOR: {}".format(" ".join(connections[i].Q.get()))])
 
 
 if __name__ == '__main__':
-    input_q = Queue()  # multiprocessing Q for clients' inputs
-    output_q = Queue()  # multiprocessing Q for ticks' results
-    server = Process(name="server", target=server,  args=(input_q, output_q, ))
-    tick = Process(name="tick", target=tick, args=(input_q, output_q, ))
+    connections = Manager().dict()  # A dict for saving connections
+    output_q = Manager().Queue()  # multiprocessing Q for ticks' results
+    server = Process(name="server", target=server,
+                     args=(connections,
+                           output_q, Manager().Queue()))
+    tick = Process(name="tick", target=tick, args=(connections, output_q, ))
 
     server.start()
     tick.start()
--- test_client.py
+++ test_client.py
@@ -10,17 +10,27 @@ async def hello():
         await websocket.send(hi)
         HI = await websocket.recv()
         print(HI)
-        await producer(websocket)
+        while True:
+            # starting two coroutines
+            a = asyncio.ensure_future(consumer(websocket))
+            b = asyncio.ensure_future(producer(websocket))
+            done, pending = await asyncio.wait(
+                [a, b],
+                return_when=asyncio.FIRST_COMPLETED,
+            )
+            # restarting both, if one returns
+            for task in pending:
+                task.cancel()
 
 
 async def consumer(websocket):
-    pass
+    message = await websocket.recv()
+    print(message)
 
 
 async def producer(websocket):
-    while True:
-        actions = input("What's happend?: ")
-        await websocket.send(actions)
+    actions = input("What's happend?: ")
+    await websocket.send(actions)
 
 
 asyncio.get_event_loop().run_until_complete(hello())
